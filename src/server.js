4ec45e35ae5376fb87e067e72d6f7ab522746862
sina <sinakabki@gmail.com>
2019-02-05 18:43:37 +0000

    port fix

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({err: err.message});
 });
 
-app.listen(4300, () => {
+app.listen(process.env.PORT || 4300, () => {
   console.log('Project set up');
 });
